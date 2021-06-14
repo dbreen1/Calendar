1289248fc8222206aa1d76ab35cb55fafc8134b1
Darren Breen <darren.m.breen@outlook.com>
2021-06-14 17:38:46 +0100

    white color

--- CalendarLib/MGCEventsRowView.m
+++ CalendarLib/MGCEventsRowView.m
@@ -56,7 +56,7 @@ static const CGFloat kCellSpacing = 2.;		// space around cells
 
 		self.contentSize = CGSizeMake(frame.size.width, 400);
 		
-		self.backgroundColor = [UIColor clearColor];
+		self.backgroundColor = [UIColor whiteColor];
 		
 		self.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
 		self.clipsToBounds = YES;
